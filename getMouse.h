852a612b40cd86ce760334f5e839b16ed6f77675
berceni74 <bberceni74@gmail.com>
2026-03-29 15:28:35 +0300

    kinda what i wanted

--- getMouse.h
+++ getMouse.h
@@ -3,12 +3,14 @@
 
 #include <fcntl.h>
 #include <libevdev-1.0/libevdev/libevdev.h>
+#include <libevdev-1.0/libevdev/libevdev-uinput.h>
 #include <dirent.h>
 #include <linux/input-event-codes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 char *getMouse();
 
--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include "getMouse.h"
+#include "virtMouse.h"
+#include <libevdev-1.0/libevdev/libevdev-uinput.h>
+#include <libevdev-1.0/libevdev/libevdev.h>
+#include <linux/input-event-codes.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -16,23 +20,36 @@ int main()
     if(mouse)
     {
         int fd = open(mouse, O_RDONLY);
+        struct libevdev_uinput *virt = virtMouse();
         libevdev_new_from_fd(fd, &dev);
 
         while (1) 
         {
+            fflush(stdout);
             if(libevdev_next_event(dev, LIBEVDEV_READ_FLAG_BLOCKING, &ev) >= 0)
             {
                 if(ev.type == EV_KEY && ev.code == BTN_MIDDLE && ev.value == 1 && !isActive)
                 {
                     printf("Autoscroll enabled\n");
-                    fflush(stdout);
+                    
                     isActive = true;
-                }
-                else if (ev.type == EV_KEY && ev.code == BTN_MIDDLE && ev.value == 1 && isActive)
-                {
-                    printf("Autoscroll disabled\n");
-                    fflush(stdout);
-                    isActive = false;
+                    while(isActive)
+                    {
+                        if (libevdev_next_event(dev, LIBEVDEV_READ_FLAG_BLOCKING, &ev) >= 0){
+                            if(ev.type == EV_REL && ev.code == REL_Y)
+                            {
+                                printf("Scrolling... ; Value: %d\n", ev.value);
+                                libevdev_uinput_write_event(virt, EV_REL, REL_WHEEL, ev.value);
+                                libevdev_uinput_write_event(virt, EV_SYN, SYN_REPORT, 0);
+                            }
+
+                            if (ev.type == EV_KEY && ev.code == BTN_MIDDLE && ev.value == 1 && isActive)
+                            {
+                                printf("Autoscroll disabled\n");
+                                isActive = false;
+                            }
+                        }
+                    }
                 }
              }
         }
